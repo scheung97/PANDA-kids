493e74c744fb9d2b0cdf190b9a89fa1df1d3ac5f
scheung97 <spendell5@yahoo.com>
2019-07-25 18:40:52 -0400

    opens webcam and 'c' captures image

---
+++ webcam_work.py
@@ -0,0 +1,30 @@
+"""python facial recognition"""
+import numpy
+import cv2
+
+"""opens up laptop webcam"""
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+#vc = cv2.VideoCapture(0, cv2.CAP_DSHOW)#eliminate async callback warming?
+
+#define output file:
+out = 'testoutput.jpg'
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+
+    key = cv2.waitKey(10)
+    if key == ord('c'):
+        cv2.imwrite(out, frame)
+        print("image captured")
+    elif key == 27:
+        break
+
+vc.release()
+cv2.destroyAllWindows()
+"""-----"""
