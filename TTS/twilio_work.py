81cd42fb262eada9f7c7f6496f895f3512cc515d
scheung97 <spendell5@yahoo.com>
2019-10-01 10:33:30 -0400

    Update twilio_work.py
    removed auth. token for security reasons

--- TTS/twilio_work.py
+++ TTS/twilio_work.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
 account_sid = 'AC9e086a1bb6fe2cc10891a5f544f02c44'
-auth_token = '0234b54cd937f124f369c1f99f29e1e5'
+auth_token = '[Auth Token]' 
 client = Client(account_sid, auth_token)
 
 text_content = 'Spenser is baby and needs your help'
