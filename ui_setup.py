6df285cd8c00a8ef8b290ca9313bbd6c7a075a74
lexispike <alyxandra@spikerman.net>
2019-10-18 09:38:28 -0400

    redis init

--- ui_setup.py
+++ ui_setup.py
@@ -1,11 +1,38 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from redislite import Redis
+from threading import Thread
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'MYSECRETCODE'
 socketio = SocketIO(app)
 
+
+class RedisListener(Thread):
+    def __init__(self, r, channels, flask_app):
+        Thread.__init__(self)
+
+        # Thread is killed on process end
+        self.daemon = True
+
+        # Store redis info in thread
+        self.redis = r
+        self.pubsub = self.redis.pubsub()
+
+        # Subscribe to selected channels
+        self.pubsub.psubscribe(channels)
+
+        # Store flask app info in thread
+        self.app = flask_app
+
+    def handle_event(event):
+        # Handle the events here!
+        # figures out what event is what
+        pass
+
+    def run(self):
+        for event in self.pubsub.listen():
+            self.handle_event(event)
 
 @app.route('/')
 def selection():
@@ -25,4 +52,8 @@ def settings():
 
 
 if __name__ == '__main__':
+    r = redis.Redis()
+    client = Listener(r, ['channels'], app)
+    client.start()
+
     socketio.run(app)
